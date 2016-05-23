23cc24d904ddbdd37ab3921e95122e6d07716dca
Victor Viale <somebodywas@gmail.com>
2016-05-23 21:54:23 +0200

    Rename components

---
+++ public/src/components/CommentComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="comment">
+    <div class="info">
+      <span class="date"></span>
+      <span class="author"></span>
+    </div>
+    <div class="text">
+      
+    </div>
+
+    <div class="sub-comments">
+      
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CommentComponent",
+    props: {
+      comment: Object
+    },
+    data () {
+      return {
+        childComments: []
+      }
+    },
+    created () {
+      if (this.comment.childComments) {
+        let ids = _.map(this.comment.childComments, _.property('id'))
+
+      }
+    }
+  }
+</script>
---
+++ public/src/components/PostComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="post">
+    <img src="" alt="">
+    <h1 class="titre"></h1>
+    <div class="text">
+      
+    </div>
+  </article>
+</template>
+
+<script>
+  export default {
+    name: "PostComponent",
+    props: {
+      post: Object
+    },
+    data () {
+      return {
+        comments: []
+      }
+    }
+  }
+
+
+</script>
