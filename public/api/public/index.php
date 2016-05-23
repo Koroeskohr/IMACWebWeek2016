c78b832aaea8140d771a74f33082566d1a69f532
Victor Viale <somebodywas@gmail.com>
2016-05-23 22:02:19 +0200

    Fix CORS

--- public/api/public/index.php
+++ public/api/public/index.php
@@ -8,7 +8,7 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Max-Age: 86400');    // cache for 1 day
 
