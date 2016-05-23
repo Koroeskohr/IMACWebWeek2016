5da298f847aed9e32ab0166f3169cc267dac06c8
Matthieu <matthieubessol@gmail.com>
2016-05-23 15:37:37 +0200

    Start routes

--- public/api/src/routes.php
+++ public/api/src/routes.php
@@ -16,6 +16,38 @@ $app->get('/route/{id}', function($request, $response, $args) {
   return $response->withJson($array);
 });
 
+// topic/*/posts
+$app->get('/topic/{id}/posts', function($request, $response, $args) {
+    $sql = 'SELECT post_id FROM posts WHERE id_topic {id}';
+    $array = $this->db->query($sql);
+    return $response->withJson($array);
+});
+
+// /post/*/comments
+$app->get('/post/{id}/comments/', function($request, $response, $args) {
+    $sql = 'SELECT id_comment FROM comments WHERE id_post {id}';
+    $array = $this->db->query($sql);
+    return $response->withJson($array);
+});
+
+// /topics GET
+$app->get('/topics/', function($request, $response, $args) {
+    $sql = 'SELECT * FROM topics';
+    $array = $this->db->query($sql);
+    return $response->withJson($array);
+});
+
+// /topics POST
+$app->post('/topics/', function ($request, $response, $args) {
+    $array = $app->request->post();
+    return $array;
+});
+
+// /topics PATCH
+// $app->patch('/topics/', function($request, $response, $args) {
+
+// });
+
 $app->get('/jsonTestRoute', function($request, $response, $args) {
   $array = [ "key" => "value" ];
   return $response->withJson($array);
