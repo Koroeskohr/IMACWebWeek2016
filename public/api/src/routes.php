4281d6dd32e5be0568c804b79af374b5b5daae22
joyahd <joyahaddad@Joyas-MacBook-Pro.local>
2016-05-23 16:17:46 +0200

    ca marche pour tag id posts

--- public/api/src/routes.php
+++ public/api/src/routes.php
@@ -61,9 +61,10 @@ $app->get('/jsonTestRoute', function($request, $response, $args) {
 //jointure
 
 $app->get('/tag/{id}/posts', function($request, $response, $args) {
-  $sql = 'SELECT * FROM posts INNER JOIN tags ON posts.id=tags.id}';
-  $array = $this->db->query($sql);
-  return $response->withJson($array);
+  $sql = 'SELECT * FROM Post INNER JOIN Tagge ON idPost=idTag';
+  $query = $this->db->query($sql);
+  $result = $query->fetchAll();
+  return $response->withJson($result);
 });
 
 //jointure
@@ -72,38 +73,43 @@ $app->get('/tag/{id}/posts', function($request, $response, $args) {
 
 $app->get('/topics/{id}/', function($request, $response, $args) {
   $sql = 'SELECT id FROM topics WHERE id_topics {id}';
-    $array = $this->db->query($sql);
-  return $response->withJson($array);
+    $query = $this->db->query($sql);
+    $result = $query->fetchAll();
+  return $response->withJson($result);
 });
 
 // tags sur le post
 
 $app->get('/post/{id}/tags/', function($request, $response, $args) {
-  	$sql = 'SELECT tags FROM posts WHERE id_tags {id}';
-   	$array = $this->db->query($sql);
-  return $response->withJson($array);
+  	$sql = 'SELECT Tag FROM Post WHERE id_tags {id}';
+   	$query = $this->db->query($sql);
+   	$result = $query->fetchAll();
+  return $response->withJson($result);
 });
 
 
 // post - creer un post
 $app->post('/tags/', function($request, $response, $args) {
-	$sql = 'SELECT * FROM tags' ;
-    $array = $app->request->post();
-  return $response->withJson($array);
+	$sql = 'SELECT * FROM Tag' ;
+    $query = $app->request->post();
+    $result = $query->fetchAll();
+  return $response->withJson($result);
 });
 
 
 // update post
 
 $app->patch('/post/{id}/tags/', function($request, $response, $args) {
-  $array = [ "key" => "value" ];
-  return $response->withJson($array);
+  $query = [ "key" => "value" ];
+   $result = $query->fetchAll();
+  return $response->withJson($result);
 });
 
 //delete post
 $app->delete('/post/{id}/tags/', function($request, $response, $args) {
-  $array = [ "key" => "value" ];
-  return $response->withJson($array);
+  $query = [ "key" => "value" ];
+    $result = $query->fetchAll();
+  return $response->withJson($result);
 });
 
 
