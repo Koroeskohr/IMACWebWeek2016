829b2ee9a3aa6ce89d84d8d903c73ce11cf701b9
Victor Viale <somebodywas@gmail.com>
2016-05-23 21:55:35 +0200

    Add basic ListView implementation

--- public/src/views/ListView.vue
+++ public/src/views/ListView.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="home">
     ListView
+    
+    <post-component
+      v-for='post in posts'
+      :post='post'>
+    </post-component>
+
   </div>
 </template>
+
+<script>
+  import PostComponent from '../components/PostComponent.vue'
+  import CommentComponent from '../components/CommentComponent.vue'
+
+  export default {
+    name: "ListView",
+    components: { PostComponent, CommentComponent },
+    data () {
+      return {
+        topic_id: 0,
+        posts: []
+      }
+    },
+    route: {
+      data ({ to }) {
+        //to.params.id = topic_id
+        this.$http.get('topic/'+ to.params.id +'/posts').then( 
+          (response) => {
+            this.topic_id = to.params.id
+            console.log(response)
+          }, (response) => {
+            console.log("post all fail " + response)
+          }
+        )
+      }
+    }
+  }
+</script>
