12030bf871164ab1bea3bad5665e5d0d51892bb1
Victor Viale <somebodywas@gmail.com>
2016-05-24 10:40:37 +0200

    Add HomeView implementation

--- public/src/views/HomeView.vue
+++ public/src/views/HomeView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="home">
     HomePage
+
+
   </div>
 </template>
+
+<script>
+  export default {
+    name: "HomeView",
+    data () {
+      return {
+        topics: []
+      }
+    },
+    route: {
+      data ({to}) {
+        this.$http.get('topics').then(
+          (response) => {
+            this.topics = response.data
+          },
+          (response) => {
+            console.log("topics all fail" + response);
+          }
+
+        )
+      }
+    }
+  }
+</script>
