49c455f47cdd1ac8e76df2e2d20865444d21fadc
DKroco <vaness.bar@hotmail.fr>
2016-05-26 13:09:59 +0200

    finish post model

--- public/api/src/models/Post.php
+++ public/api/src/models/Post.php
@@ -9,11 +9,7 @@ class Post {
 
 	public function create($titre, $auteur, $image, $contenu, $idSubject) {
 		try {
-			//if (empty($image))
-			//	$sql = "INSERT INTO Post (`titre`, `auteur`, `image`, `texte`, `sujet`) VALUES ('".$titre."', '".$auteur."', NULL, '".$contenu."', '".$idSubject."')";
-			//else
-				$sql = "INSERT INTO Post (`titre`, `auteur`, `image`, `texte`, `sujet`) VALUES ('".$titre."', '".$auteur."', '".$image."', '".$contenu."', '".$idSubject."')";
-		    //echo $sql;
+			$sql = "INSERT INTO Post (`titre`, `auteur`, `image`, `texte`, `sujet`) VALUES ('".$titre."', '".$auteur."', '".$image."', '".$contenu."', '".$idSubject."')";
 		    $query = $this->db->query($sql);
 		    $status = 200;
 		} catch (Exception $e){
@@ -41,6 +37,27 @@ class Post {
 		$query = $this->db->query($sql);
 		$result = $query->fetchAll();
 		return $result;
+	}
+
+	public function getByTag($tagId) {
+		$sql = "SELECT * FROM Post INNER JOIN Tagge ON Tagge.idTag = ".$tagId." WHERE Tagge.idPost = Post.id;";
+		$query = $this->db->query($sql);
+		$result = $query->fetchAll();
+		return $result;
+	}
+
+	public function delete($id) {
+		$sql = "SELECT * FROM Post WHERE id = ".$id.";";
+		$query = $this->db->query($sql);
+		$result = $query->fetchAll();
+		if (count($result) == 1) {
+			$sql = "DELETE FROM Post WHERE id = ".$id;
+			$query = $this->db->query($sql);
+			$status = 200;
+		} else {
+			$status = 400;
+		}
+		return $status;
 	}
 }
 
