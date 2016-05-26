b3c0d0c8eec360d9cc2710088b64ee47d7f718b3
DKroco <vaness.bar@hotmail.fr>
2016-05-26 12:40:01 +0200

    Continue post models

--- public/api/src/models/Post.php
+++ public/api/src/models/Post.php
@@ -1,12 +1,25 @@
 <?php
 
-require 'Database.php';
-
 class Post {
 	private $db;
 
 	public function __construct($db) {
 		$this->db = $db;
+	}
+
+	public function create($titre, $auteur, $image, $contenu, $idSubject) {
+		try {
+			if (empty($image))
+				$sql = "INSERT INTO Post (`titre`, `auteur`, `image`, `texte`, `sujet`) VALUES ('".$titre."', '".$auteur."', NULL, '".$contenu."', '".$idSubject."')";
+			else
+				$sql = "INSERT INTO Post (`titre`, `auteur`, `image`, `texte`, `sujet`) VALUES ('".$titre."', '".$auteur."', '".$image."', '".$contenu."', '".$idSubject."')";
+		    echo $sql;
+		    $query = $this->db->query($sql);
+		    $status = 200;
+		} catch (Exception $e){
+		    $status = 400;
+		}
+		return $status;
 	}
 
 	public function getById($id) {
@@ -18,6 +31,13 @@ class Post {
 
 	public function getAll() {
 		$sql = "SELECT * FROM Post";
+		$query = $this->db->query($sql);
+		$result = $query->fetchAll();
+		return $result;
+	}
+
+	public function getBySubject($subjectId) {
+		$sql = "SELECT * FROM Post WHERE sujet =".$subjectId.";";
 		$query = $this->db->query($sql);
 		$result = $query->fetchAll();
 		return $result;
