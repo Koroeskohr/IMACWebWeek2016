4e80a7df5c958ba5c2f755073e9ca4a9728a2b22
Caroline Vien <caroline.vien8@gmail.com>
2016-05-26 13:13:21 +0200

    DB ADD USER

--- db.sql
+++ db.sql
@@ -13,11 +13,13 @@ CREATE TABLE `Comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `texte` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `auteur` varchar(255) NOT NULL,
+  `auteur` int(10) unsigned NOT NULL,
   `post` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `post` (`post`),
   KEY `reponse` (`reponse`),
+  KEY `auteur` (`auteur`),
+  CONSTRAINT `Comments_ibfk_5` FOREIGN KEY (`auteur`) REFERENCES `User` (`id`),
   CONSTRAINT `Comments_ibfk_3` FOREIGN KEY (`post`) REFERENCES `Post` (`id`) ON DELETE CASCADE,
   CONSTRAINT `Comments_ibfk_4` FOREIGN KEY (`reponse`) REFERENCES `Comments` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -25,25 +27,27 @@ CREATE TABLE `Comments` (
 
 DROP TABLE IF EXISTS `Post`;
 CREATE TABLE `Post` (
+  `auteur` int(10) unsigned NOT NULL,
   `titre` varchar(255) NOT NULL,
   `sujet` int(10) unsigned NOT NULL,
   `likes` int(10) unsigned NOT NULL DEFAULT '0',
-  `auteur` varchar(255) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `image` text NOT NULL,
   `texte` text NOT NULL,
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `sujet` (`sujet`),
-  CONSTRAINT `Post_ibfk_2` FOREIGN KEY (`sujet`) REFERENCES `Sujet` (`id`) ON DELETE CASCADE
+  KEY `auteur` (`auteur`),
+  CONSTRAINT `Post_ibfk_5` FOREIGN KEY (`auteur`) REFERENCES `User` (`id`),
+  CONSTRAINT `Post_ibfk_4` FOREIGN KEY (`sujet`) REFERENCES `Sujet` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Post` (`titre`, `sujet`, `likes`, `auteur`, `date`, `image`, `texte`, `id`) VALUES
-('Pique',	4,	0,	'Moi',	'2016-05-23 13:37:04',	'http://www.chicagonow.com/wild-wisdom/files/2013/11/biddy-hedgehog-16.jpg',	'Bacon ipsum dolor amet swine corned beef ground round, pork chop turkey short ribs pig salami short loin. Bresaola shoulder picanha hamburger capicola shank. Hamburger ball tip kevin landjaeger ribeye ham hock alcatra. Beef ribs pork chop pork loin, kevin picanha swine tail doner porchetta brisket drumstick jowl pork belly tri-tip. Porchetta filet mignon picanha, ground round chicken short loin beef ribs ham turducken frankfurter pork loin',	1),
-('Titre',	2,	0,	'Anon',	'2016-05-23 13:38:20',	'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/hedgehog-closeup.jpg',	'Heeeey ! I like hedgehogs ;-)',	2),
-('Zbrrrra',	3,	0,	'Zbrrrra',	'2016-05-23 13:38:59',	'http://animals.sandiegozoo.org/sites/default/files/juicebox_slides/hedgehog_05.jpg',	'Je ne sais pas vraiment de quoi parler du coup je tape sans regarder mon clavier, mais bon, je garantis rien. ',	3),
-('J\'aime les h',	1,	8000,	'K4k0',	'2016-05-23 13:40:06',	'https://pixabay.com/static/uploads/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg',	'Trolololllllll',	4),
-('Nous on aime encore plus les h',	1,	4294967295,	'S4t4N',	'2016-05-23 13:40:44',	'https://www.daysoftheyear.com/wp-content/images/hedgehog-day1-e1422787687319-804x382.jpg',	'Mouhahahahahahahahahahaha 3:D',	5);
+INSERT INTO `Post` (`auteur`, `titre`, `sujet`, `likes`, `date`, `image`, `texte`, `id`) VALUES
+(1,	'Pique',	4,	0,	'2016-05-23 13:37:04',	'http://www.chicagonow.com/wild-wisdom/files/2013/11/biddy-hedgehog-16.jpg',	'Bacon ipsum dolor amet swine corned beef ground round, pork chop turkey short ribs pig salami short loin. Bresaola shoulder picanha hamburger capicola shank. Hamburger ball tip kevin landjaeger ribeye ham hock alcatra. Beef ribs pork chop pork loin, kevin picanha swine tail doner porchetta brisket drumstick jowl pork belly tri-tip. Porchetta filet mignon picanha, ground round chicken short loin beef ribs ham turducken frankfurter pork loin',	1),
+(1,	'Titre',	2,	0,	'2016-05-23 13:38:20',	'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/hedgehog-closeup.jpg',	'Heeeey ! I like hedgehogs ;-)',	2),
+(1,	'Zbrrrra',	3,	0,	'2016-05-23 13:38:59',	'http://animals.sandiegozoo.org/sites/default/files/juicebox_slides/hedgehog_05.jpg',	'Je ne sais pas vraiment de quoi parler du coup je tape sans regarder mon clavier, mais bon, je garantis rien. ',	3),
+(1,	'J\'aime les h',	1,	8000,	'2016-05-23 13:40:06',	'https://pixabay.com/static/uploads/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg',	'Trolololllllll',	4),
+(1,	'Nous on aime encore plus les h',	1,	4294967295,	'2016-05-23 13:40:44',	'https://www.daysoftheyear.com/wp-content/images/hedgehog-day1-e1422787687319-804x382.jpg',	'Mouhahahahahahahahahahaha 3:D',	5);
 
 DROP TABLE IF EXISTS `Sujet`;
 CREATE TABLE `Sujet` (
@@ -105,4 +109,18 @@ INSERT INTO `Tagge` (`idPost`, `idTag`) VALUES
 (5,	7),
 (5,	10);
 
--- 2016-05-26 09:55:47
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `password` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `password` (`password`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `User` (`password`, `mail`, `username`, `id`) VALUES
+('PDJFIOHVHV',	'MAIHDJVHD',	'PROUT',	1);
+
+-- 2016-05-26 11:01:35
